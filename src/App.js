b1cad63c45c2bdd028a069ce9ef317ab4b8ec3e7
Kelsie Harris <kharris835@gmail.com>
2023-02-25 13:06:26 -0700

    feat: initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
