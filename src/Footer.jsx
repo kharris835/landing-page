14bde050c03b2acf4144503058e18731bf404399
Kelsie Harris <kharris835@gmail.com>
2023-02-25 16:51:10 -0700

    style: change background

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mt-2">
+    <footer className="mt-2 ps-1">
       Open-source code by Kelsie Harris with React ☕️
     </footer>
   );
