9c78b5de87b24b6c8aa236f1393ef9d763be0c9d
Kelsie Harris <kharris835@gmail.com>
2023-02-25 16:44:10 -0700

    feat: plan components and style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
+import MainContent from "./MainContent";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <header className="App-header"></header>
+      <div className="container">
+        <div className=" content card mt-5">
+          <NavBar />
+          <main>
+            <MainContent />
+          </main>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
