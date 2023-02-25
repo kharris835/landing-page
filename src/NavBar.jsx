dcbfde3600f31a7649f9aac99ed3d77d37938ae2
Kelsie Harris <kharris835@gmail.com>
2023-02-25 16:44:42 -0700

    feat: add nav bar and style

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import "./NavBar.css";
 
 export default function NavBar() {
-  return <div>Hello from NavBar</div>;
+  return (
+    <nav className="d-flex align-items-center justify-content-between">
+      <span className="d-flex align-items-center">
+        <img src="penguin.png" width="70px" alt="React logo" />
+        <h3 className="mb-0">PenguinFacts</h3>
+      </span>
+      <h6 className="mb-0 me-2">Show Your Support</h6>
+    </nav>
+  );
 }
