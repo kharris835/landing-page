8e2160b80b609cfa9ba69de43cf740c868951be3
Kelsie Harris <kharris835@gmail.com>
2023-02-25 16:44:30 -0700

    feat: move footer to component

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mt-2">
+      Open-source code by Kelsie Harris with React ☕️
+    </footer>
+  );
+}
