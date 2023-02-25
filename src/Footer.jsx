dcb40e0a801f95c77b0a0cc15206387f20b9dff7
Kelsie Harris <kharris835@gmail.com>
2023-02-25 16:56:08 -0700

    feat: link to code and personal website

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,20 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="mt-2 ps-1">
-      Open-source code by Kelsie Harris with React ☕️
+      <a
+        href="https://sparkling-parfait-abd00f.netlify.app/"
+        className="text-decoration-none"
+      >
+        Open-source{" "}
+      </a>
+      code by{" "}
+      <a
+        href="https://spontaneous-croquembouche-c44b06.netlify.app/"
+        className="text-decoration-none"
+      >
+        Kelsie Harris{" "}
+      </a>
+      with React ☕️
     </footer>
   );
 }
