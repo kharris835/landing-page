7b1623553ffb56e59f96b76a0479de45d1cfe178
Kelsie Harris <kharris835@gmail.com>
2023-02-25 17:27:01 -0700

    feat: media query of nav bar for mobile and support link

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mt-2 ps-1">
+    <footer className="mt-2 mb-2 ps-1">
       <a
         href="https://sparkling-parfait-abd00f.netlify.app/"
         className="text-decoration-none"
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -8,7 +8,17 @@ export default function NavBar() {
         <img src="penguin.png" width="70px" alt="React logo" />
         <h3 className="mb-0">PenguinFacts</h3>
       </span>
-      <h6 className="mb-0 me-2">Show Your Support</h6>
+      <h6 className="mb-0 me-2">
+        <a
+          href="https://support.worldwildlife.org/site/Donation2?df_id=14597&14597.donation=form1&s_src=AWE2006OQ18492A03648RX&gclid=Cj0KCQiAgOefBhDgARIsAMhqXA5fyOIHkMJ-QSjVtAKXXqH_ME-kowgsmETny9Zy_yOps7VCBDrVMGIaAn1yEALw_wcB"
+          className="support-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="full-text">Show Your Support</span>
+          <span className="short-text">Support</span>
+        </a>
+      </h6>
     </nav>
   );
 }
